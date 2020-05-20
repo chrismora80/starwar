556a9b4ae39d6610b79b0ee7d01ad4a0fabe35a3
Christian Mora <christianmora007@hotmail.com>
2020-05-19 21:59:48 -0400

    updates

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.ev.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
+var characters =
   {
     routeName: "yoda",
     name: "Yoda",
